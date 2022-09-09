d0b17b1aac7dd3c4b6524f882776410cdc7ac467
mafusail <mikelnord@gmail.com>
2022-09-10 05:08:18 +1000

    commit add DrawableRes&layouts

---
+++ app/src/main/java/com/android/filminfo/ui/FindFragment.kt
@@ -0,0 +1,29 @@
+package com.android.filminfo.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.filminfo.databinding.FragmentFindBinding
+
+class FindFragment : Fragment() {
+
+    private var _binding: FragmentFindBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentFindBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
